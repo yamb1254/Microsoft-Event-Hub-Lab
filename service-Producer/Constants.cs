d01864d271c54af9b311acbb41f7b53404b71bb4
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2025-01-15 14:45:20 +0200

    Delete service-Producer directory

--- service-Producer/Constants.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class Constants
-{
-    public const string EventHubNamespaceEnv = "EVENT_HUB_NAMESPACE"; 
-    public const string EventHubNameEnv = "EVENT_HUB_NAME";
-    public static readonly TimeSpan EventSendDelay = TimeSpan.FromMinutes(1);
-    public static readonly string[] DefaultMessages = { "Message 1", "Message 2", "Message 3" };
-}
