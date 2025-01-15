f256c8847571f74600f298ca9ac83f6861359950
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2025-01-15 14:45:11 +0200

    Delete service-Consumer directory

--- service-Consumer/Constants.cs
+++
@@ -1,6 +0,0 @@
-public static class Constants
-{
-    public const string EventHubNamespaceEnv = "EVENT_HUB_NAMESPACE";
-    public const string EventHubNameEnv = "EVENT_HUB_NAME";
-    public const string DefaultConsumerGroup = "$Default"; 
-}
